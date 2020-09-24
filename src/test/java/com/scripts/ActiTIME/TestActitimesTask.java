66877ca44e3d7bca432ff2211caea1777262c0dc
fzkha <fzkha@192.168.2.10>
2020-09-24 17:41:24 -0300

    new file added

---
+++ src/test/java/com/scripts/ActiTIME/TestActitimesTask.java
@@ -0,0 +1,15 @@
+package com.scripts.ActiTIME;
+
+import org.testng.annotations.Test;
+
+public class TestActitimesTask {
+	@Test
+	
+	public void output()
+	{
+		System.out.println("New CLass");
+	}
+	
+	
+
+}
